f7774368b16c8162148dcff402237fc59b66b50a
ankur9301 <ankurgyawali9301@gmail.com>
2026-05-15 13:58:27 -0400

    cors fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,6 +34,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://ankur9301.github.io",
+        "https://project-map-red.vercel.app/"
     ],
     allow_origin_regex=r"chrome-extension://.*",
     allow_credentials=True,
